7732bad67315a8137fcdc8453148088e929f5d4b
Anthony-7k <2652424619@qq.com>
2026-08-05 12:22:11 +0800

    feat: add llm client interaction and update README

---
+++ agent01/README.md
@@ -0,0 +1,29 @@
+# Agent01 - LLM Client Demo
+
+
+## 项目介绍
+
+这是一个基于 Python 的大模型调用测试项目。
+
+第一阶段目标：
+
+- 搭建可复现的 Python 开发环境
+- 使用环境变量管理模型配置
+- 调用 OpenAI-compatible API
+- 实现命令行输入问题并获取模型回复
+
+
+## 环境要求
+
+- Python 3.11+
+- uv
+- OpenAI-compatible API
+
+
+## 项目启动
+
+
+### 1. 安装依赖
+
+```bash
+uv sync
--- agent01/hello_llm.py
+++ agent01/hello_llm.py
@@ -3,30 +3,45 @@ from dotenv import load_dotenv
 import os
 
 
-# 加载 .env 文件
 load_dotenv()
 
 
 def main():
+
     client = OpenAI(
         api_key=os.getenv("LLM_API_KEY"),
         base_url=os.getenv("LLM_BASE_URL")
     )
 
-    response = client.chat.completions.create(
-        model=os.getenv("LLM_MODEL"),
-        messages=[
-            {
-                "role": "user",
-                "content": "你好，请简单介绍一下你自己"
-            }
-        ],
-        temperature=0.7,
-        max_tokens=500
-    )
 
-    print("DeepSeek回复：")
-    print(response.choices[0].message.content)
+    while True:
+
+        question = input(
+            "请输入问题:"
+        )
+
+
+        if question == "exit":
+            break
+
+
+        response = client.chat.completions.create(
+            model=os.getenv("LLM_MODEL"),
+            messages=[
+                {
+                    "role":"user",
+                    "content":question
+                }
+            ],
+            temperature=0.7,
+            max_tokens=500
+        )
+
+
+        print(
+            "AI:",
+            response.choices[0].message.content
+        )
 
 
 if __name__ == "__main__":
